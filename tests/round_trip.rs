62cab1163543b24d670b203fe4632594aafe8b17
li <li@goldragon.criome.net>
2026-06-23 17:58:57 +0200

    meta-signal-spirit: use renamed schema stack crates

--- tests/round_trip.rs
+++ tests/round_trip.rs
@@ -5,7 +5,7 @@ use meta_signal_spirit::{
     CriomeSocketPathText, ImportReceipt, ImportedRecords, Input, MirrorAddress, MirrorAddressText,
     MirrorTarget, Output,
 };
-use nota_next::{NotaDecode, NotaEncode, NotaSource};
+use nota::{NotaDecode, NotaEncode, NotaSource};
 use signal_frame::SignalOperationHeads;
 use signal_spirit::schema::signal::{CommitSequence, DatabaseMarker, RecordCount, StateDigest};
 
